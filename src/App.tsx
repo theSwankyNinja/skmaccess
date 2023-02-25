0bb1d3fc9ac742bb3dfabc7a169376a0c286317b
Arthur Guo <saturnmfgg@gmail.com>
2023-02-25 16:52:11 -0600

    [AG] - React Context Example

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import * as React from 'react';
 import './App.css';
 import AppHeader from './AppHeader'
 import ExpandBox from './expandBox'
+import GlobalProvider from './provider/GlobalController';
 
 
 function App() {
   return (
     <div className="App">
-      <AppHeader />
-      <ExpandBox />
+      <GlobalProvider>
+        <AppHeader />
+        <ExpandBox />
+      </GlobalProvider>
     </div>
   );
 }
